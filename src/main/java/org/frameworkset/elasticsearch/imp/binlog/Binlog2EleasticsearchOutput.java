3f09ca89ca00f8d871e52d264c471375910b7690
yin-bp <yin-bp@163.com>
2023-06-01 17:15:18 +0800

    1.优化增量状态管理机制，处理drop记录可能导致的记录状态不正确 2.优化mysql binlog增量状态管理机制

--- src/main/java/org/frameworkset/elasticsearch/imp/binlog/Binlog2EleasticsearchOutput.java
+++ src/main/java/org/frameworkset/elasticsearch/imp/binlog/Binlog2EleasticsearchOutput.java
@@ -17,8 +17,10 @@ package org.frameworkset.elasticsearch.imp.binlog;
 
 import org.frameworkset.spi.assemble.PropertiesContainer;
 import org.frameworkset.spi.assemble.PropertiesUtil;
+import org.frameworkset.tran.DataRefactor;
 import org.frameworkset.tran.DataStream;
 import org.frameworkset.tran.config.ImportBuilder;
+import org.frameworkset.tran.context.Context;
 import org.frameworkset.tran.plugin.es.output.ElasticsearchOutputConfig;
 import org.frameworkset.tran.plugin.mysqlbinlog.input.MySQLBinlogConfig;
 import org.slf4j.Logger;
@@ -40,15 +42,19 @@ public class Binlog2EleasticsearchOutput {
         ImportBuilder importBuilder = new ImportBuilder();
         importBuilder.setBatchSize(batchSize);//设置批量入库的记录数
 
+        //binlog插件配置开始​
         MySQLBinlogConfig mySQLBinlogConfig = new MySQLBinlogConfig();
         mySQLBinlogConfig.setHost("192.168.137.1");
         mySQLBinlogConfig.setPort(3306);
         mySQLBinlogConfig.setDbUser("root");
         mySQLBinlogConfig.setDbPassword("123456");
-//        mySQLBinlogConfig.setFileNames("F:\\6_environment\\mysql\\binlog.000107,F:\\6_environment\\mysql\\binlog.000127");
-        mySQLBinlogConfig.setTables("cityperson");//
-        mySQLBinlogConfig.setDatabase("bboss");
+        //如果直接监听文件则设置binlog​文件路径，否则不需要配置文件路径
+        //mySQLBinlogConfig.setFileNames("F:\\6_environment\\mysql\\binlog.000107,F:\\6_environment\\mysql\\binlog.000127");
+        mySQLBinlogConfig.setTables("cityperson");//监控增量表名称
+        mySQLBinlogConfig.setDatabase("bboss");//监控增量表名称
         mySQLBinlogConfig.setServerId(65536L);
+        //binlog插件配置结束​
+        importBuilder.setInputConfig(mySQLBinlogConfig);
         importBuilder.setInputConfig(mySQLBinlogConfig);
         importBuilder.setPrintTaskLog(true);
 
@@ -65,10 +71,32 @@ public class Binlog2EleasticsearchOutput {
                 .addElasticsearchProperty("default.http.connectionRequestTimeout","70000")
                 .addElasticsearchProperty("default.http.maxTotal","200")
                 .addElasticsearchProperty("default.http.defaultMaxPerRoute","100")
-                .setIndex("dbdemo")
-                .setEsIdField("log_id");//设置文档主键，不设置，则自动产生文档id,设置好id后，binlog采集的增删改数据，会自动同步到Elasticsearch
+                .setIndex("binlogdemo")
+                .setEsIdField("rowNo");//设置文档主键，不设置，则自动产生文档id,设置好id后，binlog采集的增删改数据，会自动同步到Elasticsearch
         importBuilder.setOutputConfig(elasticsearchOutputConfig);
+        importBuilder.setDataRefactor(new DataRefactor() {
+            @Override
+            public void refactor(Context context) throws Exception {
+                int action = (int)context.getMetaValue("action");
+                if(context.isUpdate()) {
+//                    context.setDrop(true); //丢弃修改和删除数据
+                    logger.info("isUpdate");
+                }
 
+                if(context.isDelete()) {
+//                    context.setDrop(true); //丢弃修改和删除数据
+                    logger.info("isDelete");
+                }
+                /**
+                String table = (String)context.getMetaValue("table");
+                if(table.equals("cityperson"))
+                    context.setIndex("cityperson-{dateformat=yyyy.MM.dd}");
+                else
+                    context.setIndex("batchtest-{dateformat=yyyy.MM.dd}");
+                 */
+//                int action1 = (int)context.getMetaValue("action1");
+            }
+        });
         DataStream dataStream = importBuilder.builder();
         dataStream.execute();
     }
