ae0d79baf73e081b0a592b7422e024b78a7c6a04
yin-bp <yin-bp@163.com>
2023-08-15 12:52:10 +0800

    增加db到doris同步案例

---
+++ src/main/java/org/frameworkset/elasticsearch/imp/doris/Db2Dorisdemo.java
@@ -0,0 +1,289 @@
+package org.frameworkset.elasticsearch.imp.doris;
+/**
+ * Copyright 2008 biaoping.yin
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.frameworkset.util.SimpleStringUtil;
+import org.frameworkset.elasticsearch.serial.SerialUtil;
+import org.frameworkset.spi.geoip.IpInfo;
+import org.frameworkset.tran.DataRefactor;
+import org.frameworkset.tran.DataStream;
+import org.frameworkset.tran.ExportResultHandler;
+import org.frameworkset.tran.ImportStartAction;
+import org.frameworkset.tran.config.ImportBuilder;
+import org.frameworkset.tran.context.Context;
+import org.frameworkset.tran.context.ImportContext;
+import org.frameworkset.tran.metrics.TaskMetrics;
+import org.frameworkset.tran.plugin.db.input.DBInputConfig;
+import org.frameworkset.tran.plugin.db.output.DBOutputConfig;
+import org.frameworkset.tran.schedule.CallInterceptor;
+import org.frameworkset.tran.schedule.ImportIncreamentConfig;
+import org.frameworkset.tran.schedule.TaskContext;
+import org.frameworkset.tran.task.TaskCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * <p>Description: 同步处理程序，如需调试同步功能，
+ * 请运行测试用例DbdemoTest中调试</p>
+ * <p></p>
+ * <p>Copyright (c) 2018</p>
+ * @Date 2018/9/27 20:38
+ * @author biaoping.yin
+ * @version 1.0
+ */
+public class Db2Dorisdemo {
+	private static final Logger logger = LoggerFactory.getLogger(Db2Dorisdemo.class);
+	public static void main(String[] args){
+		Db2Dorisdemo dbdemo = new Db2Dorisdemo();
+//		dbdemo.fullImportData(  dropIndice);
+//		dbdemo.scheduleImportData(dropIndice);
+		dbdemo.scheduleImportData();
+//		dbdemo.scheduleImportData(dropIndice);
+	}
+
+	/**
+	 * elasticsearch地址和数据库地址都从外部配置文件application.properties中获取，加载数据源配置和es配置
+	 */
+	public void scheduleImportData(){
+		ImportBuilder importBuilder = ImportBuilder.newInstance();
+
+
+
+		//指定导入数据的sql语句，必填项，可以设置自己的提取逻辑，
+		// 设置增量变量log_id，增量变量名称#[log_id]可以多次出现在sql语句的不同位置中，例如：
+		// select * from td_sm_log where log_id > #[log_id] and parent_id = #[log_id]
+		// 需要设置setLastValueColumn信息log_id，
+		// 通过setLastValueType方法告诉工具增量字段的类型，默认是数字类型
+
+
+		/**
+		 * 源db相关配置
+		 */
+		DBInputConfig dbInputConfig = new DBInputConfig();
+		dbInputConfig.setDbName("source")
+				.setDbDriver("com.mysql.cj.jdbc.Driver") //数据库驱动程序，必须导入相关数据库的驱动jar包
+				.setDbUrl("jdbc:mysql://localhost:3306/bboss?useUnicode=true&characterEncoding=utf-8&useSSL=false")
+				.setDbUser("root")
+				.setDbPassword("123456")
+				.setValidateSQL("select 1")
+				.setUsePool(true)//是否使用连接池
+				.setSqlFilepath("sql.xml")
+				.setSqlName("demoexport");
+		importBuilder.setInputConfig(dbInputConfig);
+
+		DBOutputConfig dbOutputConfig = new DBOutputConfig();
+		dbOutputConfig.setDbName("target")
+				.setDbDriver("com.mysql.cj.jdbc.Driver") //数据库驱动程序，必须导入相关数据库的驱动jar包
+				.setDbUrl("jdbc:mysql://10.13.6.7:29030/testdb")
+				.setDbUser("root")
+				.setDbPassword("")
+				.setValidateSQL("select 1")
+				.setUsePool(true)//是否使用连接池
+                .setMaxWait(300000)
+                .setConnectionTimeout(200000)
+				.setSqlFilepath("sql.xml")
+				.setInsertSqlName("insertDorisSql");
+		importBuilder.setOutputConfig(dbOutputConfig);
+
+		//在任务数据抽取之前做一些初始化处理，例如：通过删表来做初始化操作
+		importBuilder.setImportStartAction(new ImportStartAction() {
+			/**
+			 * 所有初始化操作完成后，导出数据之前执行的操作
+			 * @param importContext
+			 */
+			@Override
+			public void afterStartAction(ImportContext importContext) {
+//				try {
+//					SQLExecutor.deleteWithDBName("target","delete from tablename");//删目标库target的tablename表中的数据
+//					SQLExecutor.deleteWithDBName("target","drop table tablename"); //删表目标库target的tablename表
+//					SQLExecutor.updateWithDBName("target","create table tablename ...."); //重新创建目标库target的tablename表
+//
+//				} catch (SQLException throwables) {
+//					throwables.printStackTrace();
+//				}
+			}
+			/**
+			 * 初始化之前执行的处理操作，比如后续初始化操作、数据处理过程中依赖的资源初始化
+			 * @param importContext
+			 */
+			@Override
+			public void startAction(ImportContext importContext) {
+			}
+
+
+		});
+		importBuilder.setBatchSize(10); //可选项,批量导入db的记录数，默认为-1，逐条处理，> 0时批量处理
+		//定时任务配置，
+		importBuilder.setFixedRate(false)//参考jdk timer task文档对fixedRate的说明
+//					 .setScheduleDate(date) //指定任务开始执行时间：日期
+				.setDeyLay(1000L) // 任务延迟执行deylay毫秒后执行
+				.setPeriod(5000L); //每隔period毫秒执行，如果不设置，只执行一次
+		//定时任务配置结束
+//
+//		//设置任务执行拦截器，可以添加多个，定时任务每次执行的拦截器
+		importBuilder.addCallInterceptor(new CallInterceptor() {
+			/**
+			 * 每次定时执行任务时进行的操作
+			 * @param taskContext
+			 */
+			@Override
+			public void preCall(TaskContext taskContext) {
+//				try {
+//					SQLExecutor.deleteWithDBName("target","delete from tablename");//删目标库target的tablename表中的数据
+//					SQLExecutor.deleteWithDBName("target","drop table tablename"); //删表目标库target的tablename表
+//					SQLExecutor.updateWithDBName("target","create table tablename ...."); //重新创建目标库target的tablename表
+//
+//				} catch (SQLException throwables) {
+//					throwables.printStackTrace();
+//				}
+			}
+
+			@Override
+			public void afterCall(TaskContext taskContext) {
+				logger.info("afterCall");
+			}
+
+			@Override
+			public void throwException(TaskContext taskContext, Throwable e) {
+                logger.info("throwException",e);
+			}
+		});
+
+//		//设置任务执行拦截器结束，可以添加多个
+		//增量配置开始
+		importBuilder.setLastValueColumn("log_id");//手动指定数字增量查询字段，默认采用上面设置的sql语句中的增量变量名称作为增量查询字段的名称，指定以后就用指定的字段
+//		importBuilder.setDateLastValueColumn("log_id");//手动指定日期增量查询字段，默认采用上面设置的sql语句中的增量变量名称作为增量查询字段的名称，指定以后就用指定的字段
+		importBuilder.setFromFirst(true);//setFromfirst(false)，如果作业停了，作业重启后从上次截止位置开始采集数据，
+		//setFromfirst(true) 如果作业停了，作业重启后，重新开始采集数据
+		importBuilder.setLastValueStorePath("logdb2doris_import");//记录上次采集的增量字段值的文件路径，作为下次增量（或者重启后）采集数据的起点，不同的任务这个路径要不一样
+//		importBuilder.setLastValueStoreTableName("logs");//记录上次采集的增量字段值的表，可以不指定，采用默认表名increament_tab
+		importBuilder.setLastValueType(ImportIncreamentConfig.NUMBER_TYPE);//如果没有指定增量查询字段名称，则需要指定字段类型：ImportIncreamentConfig.NUMBER_TYPE 数字类型
+		// 或者ImportIncreamentConfig.TIMESTAMP_TYPE 日期类型
+		//增量配置结束
+
+		final AtomicInteger s = new AtomicInteger(0);
+		importBuilder.setGeoipDatabase("d:/geolite2/GeoLite2-City.mmdb");
+		importBuilder.setGeoipAsnDatabase("d:/geolite2/GeoLite2-ASN.mmdb");
+		importBuilder.setGeoip2regionDatabase("d:/geolite2/ip2region.db");
+		/**
+		 * 重新设置数据结构
+		 */
+		importBuilder.setDataRefactor(new DataRefactor() {
+			public void refactor(Context context) throws Exception  {
+				//可以根据条件定义是否丢弃当前记录
+				//context.setDrop(true);return;
+//				if(s.incrementAndGet() % 2 == 0) {
+//					context.setDrop(true);
+//					return;
+//				}
+
+
+
+				context.addFieldValue("author","duoduo");
+				context.addFieldValue("title","解放");
+				context.addFieldValue("subtitle","小康");
+				context.addFieldValue("collecttime",new Date());//
+
+//				context.addIgnoreFieldMapping("title");
+				//上述三个属性已经放置到docInfo中，如果无需再放置到索引文档中，可以忽略掉这些属性
+//				context.addIgnoreFieldMapping("author");
+
+//				//修改字段名称title为新名称newTitle，并且修改字段的值
+//				context.newName2ndData("title","newTitle",(String)context.getValue("title")+" append new Value");
+				context.addIgnoreFieldMapping("subtitle");
+				/**
+				 * 获取ip对应的运营商和区域信息
+				 */
+				IpInfo ipInfo = context.getIpInfo("LOG_VISITORIAL");
+				if(ipInfo != null)
+					context.addFieldValue("ipinfo", SimpleStringUtil.object2json(ipInfo));
+				else{
+					context.addFieldValue("ipinfo", "");
+				}
+				DateFormat dateFormat = SerialUtil.getDateFormateMeta().toDateFormat();
+				Date optime = context.getDateValue("LOG_OPERTIME",dateFormat);
+				context.addFieldValue("logOpertime",optime);
+				context.addFieldValue("collecttime",new Date());
+//				对数据进行格式化
+//				DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+//				Date optime = context.getDateValue("LOG_OPERTIME");
+//
+//				context.addFieldValue("logOpertime",dateFormat.format(optime));
+
+				/**
+				 //关联查询数据,单值查询
+				 Map headdata = SQLExecutor.queryObjectWithDBName(Map.class,context.getEsjdbc().getDbConfig().getDbName(),
+				 "select * from head where billid = ? and othercondition= ?",
+				 context.getIntegerValue("billid"),"otherconditionvalue");//多个条件用逗号分隔追加
+				 //将headdata中的数据,调用addFieldValue方法将数据加入当前es文档，具体如何构建文档数据结构根据需求定
+				 context.addFieldValue("headdata",headdata);
+				 //关联查询数据,多值查询
+				 List<Map> facedatas = SQLExecutor.queryListWithDBName(Map.class,context.getEsjdbc().getDbConfig().getDbName(),
+				 "select * from facedata where billid = ?",
+				 context.getIntegerValue("billid"));
+				 //将facedatas中的数据,调用addFieldValue方法将数据加入当前es文档，具体如何构建文档数据结构根据需求定
+				 context.addFieldValue("facedatas",facedatas);
+				 */
+			}
+		});
+		//映射和转换配置结束
+		/**
+		 * 内置线程池配置，实现多线程并行数据导入功能，作业完成退出时自动关闭该线程池
+		 */
+		importBuilder.setParallel(true);//设置为多线程并行批量导入,false串行
+		importBuilder.setQueue(10);//设置批量导入线程池等待队列长度
+		importBuilder.setThreadCount(50);//设置批量导入线程池工作线程数量
+		importBuilder.setContinueOnError(true);//任务出现异常，是否继续执行作业：true（默认值）继续执行 false 中断作业执行
+
+		importBuilder
+				.setPrintTaskLog(true); //可选项，true 打印任务执行日志（耗时，处理记录数） false 不打印，默认值false
+		importBuilder.setExportResultHandler(new ExportResultHandler<String,String>() {
+			@Override
+			public void success(TaskCommand<String,String> taskCommand, String result) {
+				TaskMetrics taskMetrics = taskCommand.getTaskMetrics();
+				logger.info(taskMetrics.toString());
+			}
+
+			@Override
+			public void error(TaskCommand<String,String> taskCommand, String result) {
+				TaskMetrics taskMetrics = taskCommand.getTaskMetrics();
+				logger.info(taskMetrics.toString());
+			}
+
+			@Override
+			public void exception(TaskCommand<String,String> taskCommand, Throwable exception) {
+				TaskMetrics taskMetrics = taskCommand.getTaskMetrics();
+				logger.info(taskMetrics.toString());
+			}
+
+
+		});
+		/**
+		 * 执行数据库表数据导入es操作
+		 */
+		DataStream dataStream = importBuilder.builder();
+		dataStream.execute();//执行导入操作
+
+		logger.info("come to end.");
+
+
+	}
+
+}
