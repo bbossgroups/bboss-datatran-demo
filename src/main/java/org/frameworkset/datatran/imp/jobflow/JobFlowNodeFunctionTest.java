855748659d523c9d151bcf22ec41a418497310aa
大河 <yin-bp@163.com>
2025-06-23 18:17:48 +0800

    完善并行节点所有分支完成条件检测机制

--- src/main/java/org/frameworkset/datatran/imp/jobflow/JobFlowNodeFunctionTest.java
+++ src/main/java/org/frameworkset/datatran/imp/jobflow/JobFlowNodeFunctionTest.java
@@ -33,9 +33,10 @@ public class JobFlowNodeFunctionTest implements JobFlowNodeFunction {
 
     @Override
     public Object call(JobFlowNodeExecuteContext jobFlowNodeExecuteContext) {
-        if(true)
-            throw new RuntimeException("测试异常");
-        jobFlowNode.nodeComplete(new Exception("测试异常"));//直接完成任务，需要根据实际情况在任务处理完毕后调用节点完成方法
+//        if(true)
+//            throw new RuntimeException("测试异常");
+//        jobFlowNode.nodeComplete(new Exception("测试异常"));//直接完成任务，需要根据实际情况在任务处理完毕后调用节点完成方法
+        jobFlowNode.nodeComplete(null);
         return null;
     }
 
